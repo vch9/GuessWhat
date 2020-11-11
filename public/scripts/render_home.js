9d93a85795783bc6818cdafde79b30ebf2b0908a
vch9 <valentinchb@gmail.com>
2020-11-11 11:25:31 +0100

    Next iteration on button

--- public/scripts/render_home.js
+++ public/scripts/render_home.js
@@ -17,5 +17,22 @@ function renderImage(path) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
 
-    resizeImage(path, canvas, ctx, 1024, 1024);
+    resizeImage(path, canvas, ctx, 600, 600);
+}
+
+function renderButton(iteration) {
+    let button = document.createElement("button");
+    button.id = "next";
+    button.innerHTML = "Next iteration";
+
+    document.getElementById("game").appendChild(button);
+    document.querySelector('#next').addEventListener("click", function() {
+        iteration += 1;
+        console.log(iteration);
+    });
+}
+
+function renderHome(path, iteration) {
+    renderButton(iteration);
+    renderImage(path);
 }
