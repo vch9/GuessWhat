d2794792c5ff62fc358815e15f50f208976033e3
vch9 <valentinchb@gmail.com>
2020-11-11 14:17:42 +0100

    Server send image to clients

---
+++ src/game.js
@@ -0,0 +1,29 @@
+const current_img = {
+    image: '/public/images/god.jpg',
+    solution: 'god of war',
+    iteration: 1
+};
+
+function updateClients(players){
+    for(let i = 0; i < players.length; i++) {
+        players[i].emit('image', current_img);
+    }
+}
+
+function play(players) {
+    var interval = setInterval(function () {
+        current_img.iteration += 1;
+        updateClients(players);
+    }, 2000);
+
+    return interval;
+}
+
+exports.handleEvents = function (io) {
+    let players = [];
+    interval = play(players);
+    io.on('connection', socket => {
+        console.log('User has connected.');
+        players.push(socket);
+    });
+}
--- src/server.js
+++ src/server.js
@@ -5,9 +5,12 @@
 const http = require('http');
 const app = require('./app');
 const config = require('./config');
+const game = require('./game.js');
 
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+
+game.handleEvents(io);
 
 server.listen(config.port, () => {
     console.log("Server is listening on " + config.port);
