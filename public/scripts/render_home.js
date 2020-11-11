6f9f30ef37ab3b9b8b22d84f4f8511e223c6ad90
vch9 <valentinchb@gmail.com>
2020-11-11 11:14:16 +0100

    Resize image to 1024x1024

--- public/scripts/render_home.js
+++ public/scripts/render_home.js
@@ -1,10 +1,21 @@
-function renderImage(path) {
+function resizeImage(path, canvas, ctx, width, height) {
     var img = new Image();
     img.src = path
 
-    var ctx = document.getElementById('canvas').getContext('2d');
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        img.style.display = 'none';
+    canvas.width = width;
+    canvas.height = height;
+
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, width, height);
     }
+
+    return img
+}
+
+
+function renderImage(path) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    resizeImage(path, canvas, ctx, 1024, 1024);
 }
